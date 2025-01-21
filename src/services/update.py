9c883971d698978e8e782de5048d1a2601c7b06d
EchterTimo <EchterTimo@users.noreply.github.com>
2025-01-21 17:20:16 +0100

    made abstraction for comparing versions

--- src/services/update.py
+++ src/services/update.py
@@ -8,11 +8,14 @@ from dataclasses import dataclass
 from github import Github
 from github.GitRelease import GitRelease
 from github.GithubException import (
-    UnknownObjectException, RateLimitExceededException
+    GithubException,
+    UnknownObjectException,
+    RateLimitExceededException
 )
 
 
-__version__: str = "0.0.0"  # todo: adjust this version for release
+__version__: str = "v0.0.0"  # todo: adjust this version for release
+repo_id_for_testing: int = 373494377  # todo: remove this
 THIS_REPO_ID: int = 919969264
 '''
 the repo-id can be determined using the github api
@@ -23,7 +26,7 @@ repo by id: https://api.github.com/repositories/919969264
 
 class VersionStatus(Enum):
     """
-    Enum representing the status of a software version.
+    Enum representing the status of the software version.
     """
     PRE_RELEASE = auto()
     '''A version that is not finalized or generally available.'''
@@ -41,12 +44,20 @@ class VersionStatus(Enum):
 @dataclass
 class VersionDetails:
     '''
-    abstraction for version status, comparing and status
+    Abstraction for version status, comparing and status
     '''
 
     current_version: str
-    latest_version: str
-    latest_version_release: GitRelease  # todo: last edit
+    latest_version_release: GitRelease | None
+    fetch_error: GithubException = None
+
+    @property
+    def latest_version(self) -> str:
+        '''
+        Get the latest version
+        '''
+        if self.latest_version_release:
+            return self.latest_version_release.title
 
     @property
     def version_status(self) -> VersionStatus:
@@ -56,19 +67,41 @@ class VersionDetails:
         return determine_version_status(
             self.current_version, self.latest_version)
 
+    @classmethod
+    def get(cls, repo_id: int = THIS_REPO_ID):
+        '''
+        Get a VersionDetails instance for the current software.
+        '''
+        r, e = get_latest_release_and_error(repo_id)
+        return VersionDetails(
+            current_version=__version__,
+            latest_version_release=r,
+            fetch_error=e
+        )
 
-def get_latest_release(repo_id: int) -> GitRelease:
+    def __repr__(self) -> str:
+        return "VersionDetails(current_version='v0.0.0', latest_version_release=GitRelease(title="v1.2.1"), fetch_error=None)"
+        # todo: edit __repr__ method
+
+
+def get_latest_release_and_error(
+    repo_id: int
+) -> tuple[GitRelease, GithubException]:
     '''
     Get the latest release as a object
     '''
     try:
         g = Github(timeout=3, retry=0)
         repo = g.get_repo(repo_id)
-        return repo.get_latest_release()
-    except UnknownObjectException:
-        return None
-    except RateLimitExceededException:
-        return None
+        return repo.get_latest_release(), None
+    except UnknownObjectException as e:
+        return None, e
+    except RateLimitExceededException as e:
+        return None, e
+    except Exception as e:
+        print("Error: Unexpected Exception while retrieving Repo data")
+        print(e)
+        return None, e
 
 
 def determine_version_status(
@@ -90,4 +123,9 @@ def determine_version_status(
 
 
 if __name__ == '__main__':
-    pass
+    t = VersionDetails.get(repo_id_for_testing)
+    if not t.fetch_error:
+        print(t)
+        print(t.current_version)
+        print(t.latest_version)
+        print(t.version_status)
