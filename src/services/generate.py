ac341ff2b4659a927516321bc9027dc2afc27814
EchterTimo <EchterTimo@users.noreply.github.com>
2025-01-23 09:16:50 +0100

    finished basic implementation for generating passwords

--- src/services/generate.py
+++ src/services/generate.py
@@ -2,14 +2,30 @@
 generate passwords
 '''
 
+import os
 import random
 import string
+
+
 from dataclasses import dataclass
+import pyperclip
 
 AMBIGUOUS_CHARACTERS = set(["O", "0", "l", "1", "I", "~", "-", ",", "."])
+DOWNLOADS_FOLDER = os.path.join(os.path.expanduser("~"), "Downloads")
 
+
+@dataclass
 class Password:
+    '''
+    Class for creating and exporting passwords
+    '''
     text: str
+
+    def copy_to_clipboard(self):
+        '''
+        Copy the current password to the clipboard
+        '''
+        pyperclip.copy(self.text)
 
     @classmethod
     def generate(
@@ -21,6 +37,9 @@ class Password:
         use_special_characters: bool,
         avoid_ambiguous: bool
     ):
+        '''
+        Generate a password based on the given configuration
+        '''
         character_pool: str = ""
         if use_lower_case:
             character_pool += string.ascii_lowercase
@@ -32,7 +51,30 @@ class Password:
             character_pool += "!@#$%^&*()-_+=<>?"
         if avoid_ambiguous:
             # overwrite the list and remove ambiguous chars
-            characters = ''.join(
-                c for c in characters if c not in AMBIGUOUS_CHARACTERS)
-        
-        return random.sample(characters, length)
+            character_pool = ''.join(
+                c for c in character_pool if c not in AMBIGUOUS_CHARACTERS)
+
+        pw_text = ''.join(random.sample(character_pool, length))
+        return Password(text=pw_text)
+
+
+@dataclass
+class PasswordList:
+    '''Util class to hold multiple passwords for exporting purposes'''
+    passwords: list[Password]
+
+    def export_as_text_file(self, file_path: str = DOWNLOADS_FOLDER):
+        '''Export a list of passwords to a textfile'''
+        with open(file_path, "w", encoding="utf-8") as file:
+            file.writelines(self.passwords)
+
+
+if __name__ == '__main__':
+    pw = Password.generate(
+        length=12,
+        use_lower_case=True,
+        use_upper_case=True,
+        use_digits=True,
+        use_special_characters=True,
+        avoid_ambiguous=True,
+    )
